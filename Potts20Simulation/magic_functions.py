915175565ef757e5d20625d0d5126c9e1401ebfa
Мозоленко Вячеслав <limen2402@gmail.com>
2023-11-10 21:25:26 +0300

    Фиксация версии перед новым подходом к Поттсу

--- Potts20Simulation/magic_functions.py
+++ Potts20Simulation/magic_functions.py
@@ -1,25 +1,7 @@
 
-import mpmath as mp
-mp.mp.dps = 50
-mp.mp.pretty = True
+def magic_function(f):
+    return f+10
 
-def calc_hc_tpl(tpl):
-    st, T = tpl
-
-    E = mp.mpf(0)
-    E_sq = mp.mpf(0)
-    E_qr = mp.mpf(0)
-    Z = mp.mpf(0)
-    for fe, fS in st['S'].items():
-        e = mp.mpf(fe)
-        S = mp.mpf(fS)
-        w = mp.exp(S - e / T)
-        Z += w
-        E += w * e
-        E_sq += w * e * e
-        E_qr += w * e * e * e * e
-    avgE = E / Z;
-    avgE_sq = E_sq / Z;
-    avgE_qr = E_qr / Z;
-    # T, C, average E, BinderCumulant
-    return T, float( (avgE_sq - avgE * avgE) / (T * T) ), float( avgE ), float(1 - (avgE_qr / (3 * avgE_sq * avgE_sq)))
+def process_frame(f):
+    # changed your logic here as I couldn't repro it
+    return f, magic_function(f)
